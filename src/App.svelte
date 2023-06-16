6248ea2785332c295660281ae96e378696bf2c4a
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-06-15 23:17:44 -0500

    added color picker and style for it

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,99 @@
     </a>
   </div>
   <h1>Vite + Svelte</h1>
+    <div id="color-picker">
+      <h2>Color Picker</h2>
+      <div id="color-panel" class="grid grid-cols-5 gap-2">
+        <div
+          id="bg-slate-500"
+          class="h-12 border-2 border-black bg-slate-500"
+        />
+        <div
+          id="bg-grey-500"
+          class="w-12 h-12 border-2 border-black bg-grey-500"
+        />
+        <div
+          id="bg-zinc-500"
+          class="w-12 h-12 border-2 border-black bg-zinc-500"
+        />
+        <div
+          id="bg-neutral-500"
+          class="w-12 h-12 border-2 border-black bg-neutral-500"
+        />
+        <div
+          id="bg-stone-500"
+          class="w-12 h-12 border-2 border-black bg-stone-500"
+        />
+        <div
+          id="bg-red-500"
+          class="w-12 h-12 border-2 border-black bg-red-500"
+        />
+        <div
+          id="bg-orange-500"
+          class="w-12 h-12 border-2 border-black bg-orange-500"
+        />
+        <div
+          id="bg-amber-500"
+          class="w-12 h-12 border-2 border-black bg-amber-500"
+        />
+        <div
+          id="bg-yellow-500"
+          class="w-12 h-12 border-2 border-black bg-yellow-500"
+        />
+        <div
+          id="bg-lime-500"
+          class="w-12 h-12 border-2 border-black bg-lime-500"
+        />
+        <div
+          id="bg-green-500"
+          class="w-12 h-12 border-2 border-black bg-green-500"
+        />
+        <div
+          id="bg-emerald-500"
+          class="w-12 h-12 border-2 border-black bg-emerald-500"
+        />
+        <div
+          id="bg-teal-500"
+          class="w-12 h-12 border-2 border-black bg-teal-500"
+        />
+        <div
+          id="bg-cyan-500"
+          class="w-12 h-12 border-2 border-black bg-cyan-500"
+        />
+        <div
+          id="bg-sky-500"
+          class="w-12 h-12 border-2 border-black bg-sky-500"
+        />
+        <div
+          id="bg-blue-500"
+          class="w-12 h-12 border-2 border-black bg-blue-500"
+        />
+        <div
+          id="bg-indigo-500"
+          class="w-12 h-12 border-2 border-black bg-indigo-500"
+        />
+        <div
+          id="bg-violet-500"
+          class="w-12 h-12 border-2 border-black bg-violet-500"
+        />
+        <div
+          id="bg-purple-500"
+          class="w-12 h-12 border-2 border-black bg-purple-500"
+        />
+        <div
+          id="bg-fuchsia-500"
+          class="w-12 h-12 border-2 border-black bg-fuchsia-500"
+        />
+        <div
+          id="bg-pink-500"
+          class="w-12 h-12 border-2 border-black bg-pink-500"
+        />
+        <div
+          id="bg-rose-500"
+          class="w-12 h-12 border-2 border-black bg-rose-500"
+        />
+      </div>
+    </div>
 
   <div class="card">
     <Counter />
@@ -44,5 +137,8 @@
   }
   .read-the-docs {
     color: #888;
+
+  #color-panel > div:hover {
+    border: 4px solid black;
   }
 </style>
