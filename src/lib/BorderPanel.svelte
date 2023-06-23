0c4111ab7aef8cc77244c821ea029ae2a91aeed7
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-06-23 01:22:49 -0500

    added border styles

--- src/lib/BorderPanel.svelte
+++ src/lib/BorderPanel.svelte
@@ -4,9 +4,12 @@
   let width = "";
 </script>
 
-<div id="border-control">
+<div class="my-4" id="border-control">
   <h2>Border</h2>
+
+  <label class="label" for="border-width">Width</label>
   <select
+    class="select"
     bind:value={width}
     on:change={() => {
       border_width.update(() => width);
@@ -19,10 +22,16 @@
     <option value="border-4">4</option>
     <option value="border-8">8</option>
   </select>
-  <label for="border-width">Border-Width</label>
   <br />
 
-  <input id="border-radius" type="range" min="1" max="50" value="25" />
-  <label for="border-radius">Border-Radius</label>
+  <label class="label" for="border-radius">Border-Radius</label>
+  <input
+    class="range"
+    id="border-radius"
+    type="range"
+    min="1"
+    max="50"
+    value="25"
+  />
   <br />
 </div>
