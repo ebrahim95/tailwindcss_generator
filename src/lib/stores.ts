e22f51a6638584a44a54b2c9a6d9b2a847be57c9
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-01 16:17:55 -0500

    added the border radius store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,3 +4,4 @@ import { writable } from "svelte/store";
 export const padding = writable("");
 export const color = writable("");
 export const border_width = writable("");
+export const border_radius = writable("");
