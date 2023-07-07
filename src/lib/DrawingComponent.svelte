99ca318d9307f4c2920e09d7b127e499bde215bc
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-07 18:30:21 -0500

    removed certain props and directly imported their stores

--- src/lib/DrawingComponent.svelte
+++ src/lib/DrawingComponent.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-  export let border_radius: string;
-  export let padding: string;
-  export let bg_color: string;
-  export let border_color: string;
-  export let border_width: string;
-  export let show_code: boolean;
+    export let bg_color: string;
+    export let border_color: string;
+    export let show_code: boolean;
+
+    import { padding, border_width, border_radius, tag_to_add } from "./stores";
 </script>
 
 <div
-  id="pendu"
-  class="flex flex-col w-full place-items-center place-content-center"
+    id="pendu"
+    class="flex flex-col w-full place-items-center place-content-center"
 >
-  {#if show_code}
-    <div
-      class={`my-auto w-[400px] h-[400px] ${padding} ${bg_color} ${border_width} ${border_color} ${border_radius}`}
-    >
-      hello
-    </div>
-  {:else}
-    <div class="mockup-code">
-      <pre>
+    {#if show_code}
+        <div
+            class={`my-auto w-[400px] h-[400px] ${padding} ${bg_color} ${border_width} ${border_color} ${border_radius}`}
+        >
+            {@html $tag_to_add}
+        </div>
+    {:else}
+        <div class="mockup-code">
+            <pre>
         <code>
 
       {`my-auto w-[400px] h-[400px] ${padding} ${bg_color} ${border_width} ${border_color} ${border_radius}`}
         </code>
       </pre>
-    </div>
-  {/if}
+        </div>
+    {/if}
 </div>
