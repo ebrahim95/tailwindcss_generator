b966ce38cf5fdf490d2572c6c645f9b9ea567d33
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-10 00:12:27 -0500

    added editable_component state

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,3 +5,4 @@ export const padding = writable("");
 export const color = writable("");
 export const border_width = writable("");
 export const border_radius = writable("");
+export const editable_component = writable("");
