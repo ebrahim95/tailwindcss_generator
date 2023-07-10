787383f8e2a05e1e4c08629efd375c32c1159492
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-10 00:12:09 -0500

    added a note and variables

--- src/lib/DrawingComponent.svelte
+++ src/lib/DrawingComponent.svelte
@@ -1,29 +1,47 @@
 <script lang="ts">
-  export let bg_color: string;
-  export let border_color: string;
-  export let show_code: boolean;
+    export let show_code: boolean;
 
-  import { padding, border_width, border_radius, tag_to_add } from "./stores";
+    import {
+        padding,
+        border_width,
+        border_radius,
+        tag_to_add,
+        color,
+    } from "./stores";
+
+    let bg_color = "";
+    let border_color = "";
+
+    $: if ($color.includes("bg")) {
+        bg_color = $color;
+    } else {
+        border_color = $color;
+    }
+
+    //NOTE Maybe I should use create element for tag to add and append the variable to the class attribute
+
+    let first = `${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
+    let second = `${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
 </script>
 
 <div
-  id="pendu"
-  class="flex flex-col w-full place-items-center place-content-center"
+    id="pendu"
+    class="flex flex-col w-full place-items-center place-content-center"
 >
-  {#if show_code}
-    <div
-      class={`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`}
-    >
-      {@html $tag_to_add}
-    </div>
-  {:else}
-    <div class="mockup-code">
-      <pre>
+    {#if show_code}
+        <div
+            class={`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`}
+        >
+            {@html $tag_to_add}
+        </div>
+    {:else}
+        <div class="mockup-code">
+            <pre>
         <code>
 
       {`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`}
         </code>
       </pre>
-    </div>
-  {/if}
+        </div>
+    {/if}
 </div>
