af7c948182ec9b52ac25b3515371f025f16c2468
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-11 23:32:06 -0500

    improved the ability to edit seperate components the ability to add componenets - WIP

--- src/lib/DrawingComponent.svelte
+++ src/lib/DrawingComponent.svelte
@@ -1,47 +1,58 @@
 <script lang="ts">
-    export let show_code: boolean;
+  import { onMount } from "svelte";
+  export let show_code: boolean;
 
-    import {
-        padding,
-        border_width,
-        border_radius,
-        tag_to_add,
-        color,
-    } from "./stores";
+  import {
+    padding,
+    border_width,
+    border_radius,
+    tag_to_add,
+    color,
+    editable_component,
+  } from "./stores";
 
-    let bg_color = "";
-    let border_color = "";
+  let bg_color = "";
+  let border_color = "border-black";
 
-    $: if ($color.includes("bg")) {
-        bg_color = $color;
-    } else {
-        border_color = $color;
-    }
+  $: if ($color.includes("bg")) {
+    bg_color = $color;
+  } else {
+    border_color = $color;
+  }
 
-    //NOTE Maybe I should use create element for tag to add and append the variable to the class attribute
+  //NOTE Maybe I should use create element for tag to add and append the variable to the class attribute
 
-    let first = `${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
-    let second = `${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
+  let first = `w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
+  let second = ``;
+
+  $: second_element = document.createElement($tag_to_add);
+  $: second_element.setAttribute("class", second);
+
+  onMount(() => document.getElementById("first").appendChild(second_element));
+
+  $: document?.getElementById("first")?.appendChild(second_element);
+
+  $: if ($editable_component === "outside") {
+    first = `w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
+  } else {
+    second = `w-[200px] h-[200px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`;
+  }
 </script>
 
 <div
-    id="pendu"
-    class="flex flex-col w-full place-items-center place-content-center"
+  id="pendu"
+  class="flex flex-col w-full place-items-center place-content-center"
 >
-    {#if show_code}
-        <div
-            class={`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`}
-        >
-            {@html $tag_to_add}
-        </div>
-    {:else}
-        <div class="mockup-code">
-            <pre>
+  {#if show_code}
+    <div id="first" class={`my-auto ${first}`} />
+  {:else}
+    <div class="mockup-code">
+      <pre>
         <code>
 
       {`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`}
         </code>
       </pre>
-        </div>
-    {/if}
+    </div>
+  {/if}
 </div>
