0ea687ad343c23a3eab19a1a71c79189e8ec7337
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-16 00:32:31 -0500

    added outline store

--- src/lib/DrawingComponent.svelte
+++ src/lib/DrawingComponent.svelte
@@ -1,62 +1,70 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    export let show_code: boolean;
-    //TODO need to create Ids so that I can delete the component more easily
-    //TODO need to get a class and keep updating that specific class
+  import { onMount } from "svelte";
+  export let show_code: boolean;
+  //TODO need to create Ids so that I can delete the component more easily
+  //TODO need to get a class and keep updating that specific class
 
-    import {
-        padding,
-        border_width,
-        border_radius,
-        //  tag_to_add,
-        color,
-        // editable_component,
-        ring_width,
-    } from "./stores";
+  import {
+    padding,
+    border_width,
+    border_radius,
+    //  tag_to_add,
+    color,
+    // editable_component,
+    outline_property,
+    ring_width,
+  } from "./stores";
 
-    let bg_color = "";
-    let border_color = "border-black";
-    let ring_color = "";
+  let bg_color = "";
+  let border_color = "border-black";
+  let ring_color = "";
+  let outline_color = "";
 
-    $: if ($color.includes("bg")) {
-        bg_color = $color;
-    } else if ($color.includes("border")) {
-        border_color = $color;
-    } else {
-        ring_color = $color;
-    }
+  $: outline_property_array = Array.from($outline_property.values());
 
-    //NOTE need to rework the part
-    //let first = `w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`;
-    //let second = ``;
-    // $: second_element = document.createElement($tag_to_add);
-    // $: second_element.setAttribute("class", second);
-    // $: document?.getElementById("first")?.appendChild(second_element);
-    //
-    // $: if ($editable_component === "outside") {
-    //     first = `w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`;
-    // } else {
-    //     second = `w-[200px] h-[200px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`;
-    // }
+  $: if ($color.includes("bg")) {
+    bg_color = $color;
+  } else if ($color.includes("border")) {
+    border_color = $color;
+  } else if ($color.includes("ring")) {
+    ring_color = $color;
+  } else {
+    outline_color = $color;
+  }
+
+  //NOTE need to rework the part
+  //let first = `w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`;
+  //let second = ``;
+  // $: second_element = document.createElement($tag_to_add);
+  // $: second_element.setAttribute("class", second);
+  // $: document?.getElementById("first")?.appendChild(second_element);
+  //
+  // $: if ($editable_component === "outside") {
+  //     first = `w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`;
+  // } else {
+  //     second = `w-[200px] h-[200px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`;
+  // }
 </script>
 
 <div
-    id="pendu"
-    class="flex flex-col w-full place-items-center place-content-center"
+  id="pendu"
+  class="flex flex-col w-full place-items-center place-content-center"
 >
-    {#if show_code}
-        <div
-            id="first"
-            class={`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}`}
-        />
-    {:else}
-        <div class="mockup-code">
-            <pre>
+  {#if show_code}
+    <div
+      id="first"
+      class={`my-auto w-[400px] h-[400px]  ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color}  ${outline_property_array.join(
+        " "
+      )} ${outline_color}`}
+    />
+  {:else}
+    <div class="mockup-code">
+      <pre>
         <code>
 
-      {`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius}`}
+      {`my-auto w-[400px] h-[400px] ${$padding} ${bg_color} ${$border_width} ${border_color} ${$border_radius} ${$ring_width} ${ring_color} ${$outline_property}`}
         </code>
       </pre>
-        </div>
-    {/if}
+    </div>
+  {/if}
 </div>
