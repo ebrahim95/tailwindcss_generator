e1200454503fb7723eb2995f59d7d00645a63c19
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-17 20:05:55 -0500

    changed the ring store value from a string to a map

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -6,5 +6,5 @@ export const color = writable("");
 export const border_width = writable("");
 export const border_radius = writable("");
 export const editable_component = writable("");
-export const ring_width = writable("")
+export const ring_property = writable(new Map())
 export const outline_property = writable(new Map())
