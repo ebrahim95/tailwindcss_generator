88b67b4a9852e9c418d98aa1e3d7c663cf31de2e
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-11 21:22:45 -0500

    added regex for box shadow colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,25 +6,33 @@ const config = {
     extend: {},
   },
   safelist: [
-
+    // Padding Width
     {
       pattern: /p[a-z]-\d+/
     },
+    // Ring Colors
     {
       pattern: /ring-[a-z]+-\d+/
     },
-
+    //Border Colors and Width
     {
       pattern: /border-[a-z]+-\d+/
     },
-
+    // Border Radius, Width 
     {
       pattern: /border-[a-z]/
     },
+    // Outline Colors
     {
       pattern: /outline-[a-z]+-\d+/
-    }, {
+    },
+    // Background Colors
+    {
       pattern: /bg-[a-z]+-\d+/
+    },
+    // Shadow Colors
+    {
+      pattern: /shadow-[a-z]+-\d+/
     },
 
   ],
