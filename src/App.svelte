06fd63813f1bfe722849d7174566f16995bb0818
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-11 21:23:56 -0500

    added selection for effects panel

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-    // import svelteLogo from "./assets/svelte.svg";
-    // import viteLogo from "/vite.svg";
-    import AddHtmlComponent from "./lib/AddHtmlComponent.svelte";
-    import BorderPanel from "./lib/BorderPanel.svelte";
-    import ColorPicker from "./lib/ColorPicker.svelte";
-    import DrawingComponent from "./lib/DrawingComponent.svelte";
-    import OutlinePanel from "./lib/OutlinePanel.svelte";
-    import PaddingPanel from "./lib/PaddingPanel.svelte";
-    import RingPanel from "./lib/RingPanel.svelte";
+  // import svelteLogo from "./assets/svelte.svg";
+  // import viteLogo from "/vite.svg";
+  import AddHtmlComponent from "./lib/AddHtmlComponent.svelte";
+  import BorderPanel from "./lib/BorderPanel.svelte";
+  import ColorPicker from "./lib/ColorPicker.svelte";
+  import DrawingComponent from "./lib/DrawingComponent.svelte";
+  import EffectsPanel from "./lib/EffectsPanel.svelte";
+  import OutlinePanel from "./lib/OutlinePanel.svelte";
+  import PaddingPanel from "./lib/PaddingPanel.svelte";
+  import RingPanel from "./lib/RingPanel.svelte";
 
-    let show_code: boolean = true;
-    let panel_toggle: string = "border";
+  let show_code: boolean = true;
+  let panel_toggle: string = "border";
 
-    //NOTE Add autocompletion to the select files
-    //NOTE Maybe add skeleton ui for easy shipping
-    //NOTE Regex for decimals
-    //NOTE Where I need to edit and how will it be achieved
-    //NOTE So I need abstract the left right top and bottom
-    //NOTE abstract border and padding
+  //NOTE Add autocompletion to the select files
+  //NOTE Maybe add skeleton ui for easy shipping
+  //NOTE Regex for decimals
+  //NOTE Where I need to edit and how will it be achieved
+  //NOTE So I need abstract the left right top and bottom
+  //NOTE abstract border and padding
 </script>
 
 <!--
@@ -26,46 +27,48 @@
   Second if a simple component is made, display it's properties on the side
 -->
 <main class="flex flex-row w-12/12">
-    <section
-        class="w-3/12 flex flex-col items-start border-black border-r-4 p-4"
+  <section class="w-3/12 flex flex-col items-start border-black border-r-4 p-4">
+    <button on:click={() => (show_code = !show_code)} class="btn"
+      >Show {show_code ? "Code" : "Picture"}
+    </button>
+
+    <button />
+
+    <!-- <AddHtmlComponent /> -->
+    <ColorPicker />
+    <select
+      bind:value={panel_toggle}
+      id="panel_toggle"
+      class="select select-bordered my-3"
     >
-        <button on:click={() => (show_code = !show_code)} class="btn"
-            >Show {show_code ? "Code" : "Picture"}
-        </button>
+      <option value="border">Border</option>
+      <option value="padding">Padding</option>
+      <option value="ring">Ring</option>
+      <option value="outline">Outline</option>
+      <option value="effects">Effects</option>
+    </select>
 
-        <button />
+    <div class={`${panel_toggle === "border" ? "" : "hidden"}`}>
+      <BorderPanel />
+    </div>
+    <div class={`${panel_toggle === "padding" ? "" : "hidden"}`}>
+      <PaddingPanel />
+    </div>
+    <div class={`${panel_toggle === "ring" ? "" : "hidden"}`}>
+      <RingPanel />
+    </div>
+    <div class={`${panel_toggle === "outline" ? "" : "hidden"}`}>
+      <OutlinePanel />
+    </div>
+    <div class={`${panel_toggle === "effects" ? "" : "hidden"}`}>
+      <EffectsPanel />
+    </div>
 
-        <!-- <AddHtmlComponent /> -->
-        <ColorPicker />
-        <select
-            bind:value={panel_toggle}
-            id="panel_toggle"
-            class="select select-bordered my-3"
-        >
-            <option value="border">Border</option>
-            <option value="padding">Padding</option>
-            <option value="ring">Ring</option>
-            <option value="outline">Outline</option>
-        </select>
-
-        <div class={`${panel_toggle === "border" ? "" : "hidden"}`}>
-            <BorderPanel />
-        </div>
-        <div class={`${panel_toggle === "padding" ? "" : "hidden"}`}>
-            <PaddingPanel />
-        </div>
-        <div class={`${panel_toggle === "ring" ? "" : "hidden"}`}>
-            <RingPanel />
-        </div>
-        <div class={`${panel_toggle === "outline" ? "" : "hidden"}`}>
-            <OutlinePanel />
-        </div>
-
-        <span class="border-4 border-base-200 p-2">
-            <h1>Tailwind CSS Style Generator</h1>
-        </span>
-    </section>
-    <DrawingComponent {show_code} />
+    <span class="border-4 border-base-200 p-2 justify-self-end">
+      <h1>Tailwind CSS Style Generator</h1>
+    </span>
+  </section>
+  <DrawingComponent {show_code} />
 </main>
 
 <!-- <style>
