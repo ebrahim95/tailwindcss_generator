9aae777d0cf973a5ea919d32421c8e7d972814bf
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-11 21:22:26 -0500

    created new map value for effects property

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -10,6 +10,9 @@ export const border_width = writable("");
 export const border_radius = writable("");
 
 
-export const editable_component = writable("");
+export const editable_component = writable("")
 export const ring_property = writable(new Map())
 export const outline_property = writable(new Map())
+
+
+export const effects_property = writable(new Map())
