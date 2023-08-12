3a81c78539158d41485dd4804a81ed354443d138
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-12 00:00:36 -0500

    added mixed blend modes

--- src/lib/EffectsPanel.svelte
+++ src/lib/EffectsPanel.svelte
@@ -3,6 +3,7 @@
 
   let box_shadow_value: string = "";
   let opacity_value: string = "";
+  let mix_blend_value: string = "";
 </script>
 
 <section id="effects-panel">
@@ -55,6 +56,37 @@
       <option value="opacity-90">90</option>
       <option value="opacity-95">95</option>
       <option value="opacity-100">100</option>
+    </select>
+  </div>
+  <div class="join my-1" id="mix-blend-mode">
+    <label class="label px-3 bg-base-200 join-item" for="mix-blend">
+      Mix Blend
+    </label>
+    <select
+      class="select select-bordered join-item"
+      bind:value={mix_blend_value}
+      on:change={() =>
+        effects_property.update(() =>
+          $effects_property.set("mix-blend", mix_blend_value)
+        )}
+    >
+      <option value="mix-blend-normal">Normal</option>
+      <option value="mix-blend-multiply">Multiply</option>
+      <option value="mix-blend-screen">Screen</option>
+      <option value="mix-blend-overlay">Overlay</option>
+      <option value="mix-blend-darken">Darken</option>
+      <option value="mix-blend-lighten">Lighten</option>
+      <option value="mix-blend-color-dodge">Dodge</option>
+      <option value="mix-blend-color-burn">Burn</option>
+      <option value="mix-blend-hard-light">Hard Light</option>
+      <option value="mix-blend-soft-light">Soft Light</option>
+      <option value="mix-blend-difference">Difference</option>
+      <option value="mix-blend-exclusion">Exclusion</option>
+      <option value="mix-blend-hue">Hue</option>
+      <option value="mix-blend-saturation">Saturation</option>
+      <option value="mix-blend-color">Color</option>
+      <option value="mix-blend-luminosity">Luminosity</option>
+      <option value="mix-blend-plus-lighter">Plus Lighter</option>
     </select>
   </div>
 </section>
