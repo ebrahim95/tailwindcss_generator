8702a2ea37f17f9d86610532983ee296be76c116
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-09-27 23:23:48 -0500

    added keypress and sorted based on alphabet

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,15 @@
   let show_code: boolean = true;
   let panel_toggle: string = "border";
 
+  function value_panel_toggle(panel: string) {
+    panel_toggle = panel;
+  }
+
+  function value_panel_keydown(e: KeyboardEvent, s: string) {
+    e.preventDefault();
+    panel_toggle = s;
+  }
+
   //NOTE Add autocompletion to the select files
   //NOTE Maybe add skeleton ui for easy shipping
   //NOTE Regex for decimals
@@ -37,30 +46,48 @@
     </button>
 
     <ColorPicker />
-    <select
-      bind:value={panel_toggle}
-      id="panel_toggle"
-      class="select select-bordered mb-3"
-    >
-      <option value="border">Border</option>
-      <option value="padding">Padding</option>
-      <option value="ring">Ring</option>
-      <option value="outline">Outline</option>
-      <option value="effects">Effects</option>
-      <option value="filter">Filter</option>
-    </select>
+
+    <div class="mt-1 mb-2 mx-1 grid grid-cols-3 grid-rows-2 gap-2">
+      <button
+        on:click={() => (panel_toggle = "border")}
+        class="mb-2 mx-1 p-1 border-black border-double border-4 rounded-md ring ring-offset-4"
+      >
+        Border
+      </button>
+      <button
+        on:click={() => (panel_toggle = "padding")}
+        class="mb-2 mx-1 p-1 border-black border-double border-4 rounded-md ring ring-offset-4"
+      >
+        Padding
+      </button>
+      <button
+        on:click={() => (panel_toggle = "ring")}
+        class="mb-2 mx-1 p-1 border-black border-double border-4 rounded-md ring ring-offset-4"
+      >
+        Ring
+      </button>
+      <button
+        on:click={() => (panel_toggle = "outline")}
+        class="mb-2 mx-1 p-1 border-black border-double border-4 rounded-md ring ring-offset-4"
+      >
+        Outline
+      </button>
+      <button
+        on:click={() => (panel_toggle = "effects")}
+        class="mb-2 mx-1 p-1 border-black border-double border-4 rounded-md ring ring-offset-4"
+      >
+        Effects
+      </button>
+      <button
+        on:click={() => (panel_toggle = "filter")}
+        class="mb-2 mx-1 p-1 border-black border-double border-4 rounded-md ring ring-offset-4"
+      >
+        Filter
+      </button>
+    </div>
 
     <div class={`${panel_toggle === "border" ? "" : "hidden"}`}>
       <BorderPanel />
-    </div>
-    <div class={`${panel_toggle === "padding" ? "" : "hidden"}`}>
-      <PaddingPanel />
-    </div>
-    <div class={`${panel_toggle === "ring" ? "" : "hidden"}`}>
-      <RingPanel />
-    </div>
-    <div class={`${panel_toggle === "outline" ? "" : "hidden"}`}>
-      <OutlinePanel />
     </div>
     <div class={`${panel_toggle === "effects" ? "" : "hidden"}`}>
       <EffectsPanel />
@@ -68,13 +95,47 @@
     <div class={`${panel_toggle === "filter" ? "" : "hidden"}`}>
       <FilterPanel />
     </div>
+    <div class={`${panel_toggle === "outline" ? "" : "hidden"}`}>
+      <OutlinePanel />
+    </div>
+    <div class={`${panel_toggle === "padding" ? "" : "hidden"}`}>
+      <PaddingPanel />
+    </div>
+    <div class={`${panel_toggle === "ring" ? "" : "hidden"}`}>
+      <RingPanel />
+    </div>
     <div class="border-4 border-base-200 p-2 justify-self-end">
       <h1>Tailwind CSS Style Generator</h1>
     </div>
   </section>
+
   <DrawingComponent {show_code} />
 </main>
 
+<svelte:window
+  on:keypress|preventDefault={(e) => {
+    switch (e.key) {
+      case "b":
+        panel_toggle = "border";
+        break;
+      case "e":
+        panel_toggle = "effects";
+        break;
+      case "f":
+        panel_toggle = "filter";
+        break;
+      case "o":
+        panel_toggle = "outline";
+        break;
+      case "p":
+        panel_toggle = "padding";
+        break;
+      case "r":
+        panel_toggle = "ring";
+        break;
+    }
+  }}
+/>
 <!-- <style>
      .logo {
      height: 6em;
